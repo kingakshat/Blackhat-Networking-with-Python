acdfacb8cb61ce0c85ccdfb6177e7798f613f3de
kingakshat <akshatshrsat77@gmail.com>
2024-05-14 00:21:12 +0530

    Caesar cipher added

---
+++ Symmetric/Caesar cipher.py
@@ -0,0 +1,45 @@
+
+import random
+import string
+# ALPHABET = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# KEY = 3
+
+
+def caesar_encrypt(plain_text, key):
+
+    cipher_text = ''
+    # plain_text = plain_text.upper()
+
+    for c in plain_text:
+        index = ord(c)
+        index = (index + key) % 128
+        cipher_text += chr(index)
+
+    return cipher_text
+
+
+def caesar_decrypt(cipher_text, key):
+
+    plain_text = ''
+
+    for c in cipher_text:
+        index = ord(c)
+        index = (index - key) % 128
+        plain_text += chr(index)
+
+    return plain_text
+
+
+if __name__ == '__main__':
+
+    m = 'abc'
+
+    random_string = ''.join(random.choice(string.ascii_letters) for _ in range(16))
+    key = random_number = random.randint(1, 128)
+
+    print("String => ", random_string, "Key => ",key)
+
+    encrypted = caesar_encrypt(random_string, key)
+    print("encrypted string -> ", encrypted)
+    print("decrypted -> ",caesar_decrypt(encrypted, key))
+
